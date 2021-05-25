0904339bcb7de6c508e7fd537e249829af8d4c25
SaebomSon <newspring@gmail.com>
2021-05-26 00:52:27 +0900

    commit test

---
+++ src/baekjoon/practice/level7/Q_1316.java
@@ -0,0 +1,9 @@
+package baekjoon.practice.level7;
+
+// title : 그룹 단어 체커
+public class Q_1316 {
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/baekjoon/practice/level7/Q_2941.java
@@ -0,0 +1,10 @@
+package baekjoon.practice.level7;
+
+// title : 크로아티아 알파벳
+public class Q_2941 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
