04646cd6f7c415e2e964539fd1fea184ff77d6d5
SaebomSon <newspring56@gmail.com>
2022-01-12 17:46:19 +0900

    add q_10757

--- src/baekjoon/practice/level8/Q_10757.java
+++ src/baekjoon/practice/level8/Q_10757.java
@@ -1,6 +1,23 @@
 package baekjoon.practice.level8;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
 // Å« ¼ö A+B
 public class Q_10757 {
-
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		BigInteger bi1 = new BigInteger(st.nextToken());
+		BigInteger bi2 = new BigInteger(st.nextToken());
+		
+		bi1 = bi1.add(bi2);
+		
+		System.out.println(bi1.toString());
+		
+	}
 }
