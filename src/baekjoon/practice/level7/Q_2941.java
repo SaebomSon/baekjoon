87fadf5a0291d04ab89c967626bdbcbecd8bfb33
SaebomSon <newspring56@gmail.com>
2022-01-12 18:13:26 +0900

    add q_2941

--- src/baekjoon/practice/level7/Q_2941.java
+++ src/baekjoon/practice/level7/Q_2941.java
@@ -1,10 +1,48 @@
 package baekjoon.practice.level7;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 // title : 크로아티아 알파벳
 public class Q_2941 {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		
+		int count = 0;
+		int len = str.length();
+		
+		for(int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			
+			if(ch == 'c' && i < len - 1) {
+				if(str.charAt(i+1) == '=' || str.charAt(i+1) == '-') {
+					i++;
+				}
+			}
+			else if(ch == 'd' && i < len - 1) {
+				if(str.charAt(i+1) == 'z' && i < len - 2) {
+					if(str.charAt(i+2) == '=') {
+						i += 2;
+					}
+				}else if(str.charAt(i+1) == '-') {
+					i++;
+				}
+			}
+			else if((ch == 'l' || ch == 'n') && i < len - 1) {
+				if(str.charAt(i+1) == 'j') {
+					i++;
+				}
+			}
+			
+			else if((ch == 's' || ch == 'z') && i < len - 1) {
+				if(str.charAt(i+1) == '=') {
+					i++;
+				}
+			}
+			count++;
+		}
+		System.out.println(count);
 	}
-
 }
